9aea0e3ca94f2bd60df312e463c98a1d5dbdef13
VladimirFedoseev <fedoseev885@yandex.ru>
2022-02-02 23:55:10 +0700

    a

--- HomeWork.py
+++ HomeWork.py
@@ -1,2 +1,21 @@
-print("Hello World, I'am on GitHub")
+# print("Hello World, I'am on GitHub")
+# sep=""   убирает пробелы
 
+def hello(name, word):  # аргументы
+    print("Hello, ", name, ". Say ", word, sep="")
+
+
+hello("Irina", "hi")  # параметры
+hello("Ivan", "hello")
+
+
+def get_sum(a, b):
+    print(a + b)
+
+
+x = 2
+y = 5
+get_sum(x, y)
+get_sum("abc", "def")  # если слова то делается конкатенация
+get_sum(2.5, 4.3)
+# get_sum(2.5, "4.5")  # разные типы данных нельзя складывать
