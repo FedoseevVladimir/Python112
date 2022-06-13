354e86586e73e2dce672f52e2c1322032a0ea8e1
VladimirFedoseev <fedoseev885@yandex.ru>
2022-06-13 18:34:18 +0700

    HW

--- Work/HW.py
+++ Work/HW.py
@@ -1,48 +1,24 @@
-# ============================================================
-# HomeWork на 18.05.2022
-# import csv
-#
-# import requests
-# from bs4 import BeautifulSoup
-#
-#
-# def get_html(url):
-#     r = requests.get(url)
-#     return r.text
-#
-#
-# def write_csv(data):
-#     with open('data2.csv', 'a', encoding="utf-8") as f:
-#         writer = csv.writer(f, delimiter=";", lineterminator='\r')
-#         writer.writerow((data['name'],
-#                          data['description'],
-#                          data['price']
-#                          ))
-#
-#
-# def get_page_data(html):
-#     soup = BeautifulSoup(html, 'lxml')
-#
-#     elements = soup.find_all('a', class_="css-5l099z ewrty961")
-#     for elem in elements:
-#         name = elem.find('div', class_="css-17lk78h e3f4v4l2").text
-#         des = elem.find('div', class_="css-1fe6w6s e162wx9x0").text
-#         price = elem.find('div', class_="css-1dv8s3l eyvqki91").text
-#
-#         data = {
-#             'name': name,
-#             'description': des,
-#             'price': price
-#         }
-#
-#         write_csv(data)
-#
-#
-# def main():
-#     url = 'https://auto.drom.ru/toyota/camry/generation7/restyling0/'
-#     get_page_data(get_html(url))
-#
-#
-# if __name__ == '__main__':
-#     main()
-# ========================================================================
+import sqlite3 as sq
+
+all_clients = [
+    ('Алексей', 34, 'alex@yandex.ru'),
+    ('Ирина', 18, 'ira2004@yandex.ru'),
+    ('Евгений', 44, 'jenya@mail.ru'),
+    ('Михаил', 41, 'miha@gmail.com'),
+    ('Елена', 26, 'lena26@mail.ru'),
+    ('Светлана', 30, 'sveta13@gmail.com'),
+    ('Виктор', 36, 'vik132@yandex.ru'),
+    ('Валентина', 54, 'valya2@mail.ru'),
+    ('Артем', 33, 'artem@gmail.com')
+]
+
+with sq.connect('Clients.db') as con:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS clients(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL CHECK(age > 0 AND age < 65),
+    email TEXT UNIQUE
+    )''')
+
+    cur.executemany('INSERT INTO clients VALUES(NULL, ?, ?, ?)', all_clients)
